e12352301d7d9dad9af97d056456fb1c2754b796
Alexmathanraj shangeeth <91450175+shangeethAlex@users.noreply.github.com>
2024-11-28 23:13:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
-        main: './index.html',
-        explore: './public/explore.html',
-        brand: './public/brand.html',
-        freeguide: './public/freeguide.html',
-        guideupsell: './public/guide-upsell.html',
-        mediakit: './public/mediakit.html'
-      },
-    },
-  },
-});
+        main: '/index.html',
+        guide: '/guide.html',
+        freeguide: '/freeguide.html',
+        mediakit: '/mediakit.html',
+        explore: '/explore.html',
+        guidePro: '/guide-upsell.html'
+      }
+    }
+  }
+})
